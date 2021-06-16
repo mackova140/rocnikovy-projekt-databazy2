b8a7f56aac0c4b9b242819164db9e2b0eccee341
Natalia Mackova <79453298+mackova140@users.noreply.github.com>
2021-06-16 21:59:12 +0200

    Add files via upload

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,39 @@
+package app;
+
+import app.ui.MainMenu;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.postgresql.ds.PGSimpleDataSource;
+
+/**
+ *
+ * @author Natalia Mackova
+ */
+public class Main {
+
+    public static void main(String[] args) throws SQLException, IOException {
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+        dataSource.setServerName("www.fk-it.org");
+        dataSource.setPortNumber(5432);
+        dataSource.setDatabaseName("skusanie_mackova");
+        dataSource.setUser("mackova");
+        dataSource.setPassword("mackova1");
+
+        // System.out.println("helo");
+
+        try (Connection connection = dataSource.getConnection()) {
+            DbContext.setConnection(connection);
+
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.run();
+
+
+        } finally {
+            DbContext.clear();
+        }
+    }
+}
