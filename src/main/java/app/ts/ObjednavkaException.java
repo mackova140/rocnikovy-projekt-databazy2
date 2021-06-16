f6ecc394fe57164a7cb48cbcabe98ca18043229f
Natalia Mackova <79453298+mackova140@users.noreply.github.com>
2021-06-16 22:02:34 +0200

    Create ObjednavkaException.java

---
+++ src/main/java/app/ts/ObjednavkaException.java
@@ -0,0 +1,12 @@
+package app.ts;
+
+/**
+ *
+ * @author Mackova
+ */
+
+public class ObjednavkaException extends Exception{
+    ObjednavkaException(String message) {
+        super(message);
+    }
+}
